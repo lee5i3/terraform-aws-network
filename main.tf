a175e3baefe986f8c4268aa9dda73aae7718f13f
Lee Hull <ubuntu@openclaw.tail179985.ts.net>
2026-05-09 22:59:50 +0000

    feat: allow each subnet to specify its own cidr and availability zone

--- main.tf
+++ main.tf
@@ -20,22 +20,22 @@ resource "aws_internet_gateway" "this" {
 }
 
 resource "aws_subnet" "public" {
-  count = length(var.public_subnet_cidrs)
+  count = length(var.public_subnets)
 
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.public_subnet_cidrs[count.index]
-  availability_zone       = var.availability_zones[count.index]
+  cidr_block              = var.public_subnets[count.index].cidr
+  availability_zone       = var.public_subnets[count.index].availability_zone
   map_public_ip_on_launch = true
 
   tags = merge(local.common_tags, { Name = "${var.name}-public-${count.index + 1}" })
 }
 
 resource "aws_subnet" "private" {
-  count = length(var.private_subnet_cidrs)
+  count = length(var.private_subnets)
 
   vpc_id            = aws_vpc.this.id
-  cidr_block        = var.private_subnet_cidrs[count.index]
-  availability_zone = var.availability_zones[count.index]
+  cidr_block        = var.private_subnets[count.index].cidr
+  availability_zone = var.private_subnets[count.index].availability_zone
 
   tags = merge(local.common_tags, { Name = "${var.name}-private-${count.index + 1}" })
 }
--- variables.tf
+++ variables.tf
@@ -9,21 +9,28 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidrs" {
-  description = "List of CIDR blocks for public subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+variable "public_subnets" {
+  description = "List of public subnet configurations, each with a cidr and availability_zone"
+  type = list(object({
+    cidr              = string
+    availability_zone = string
+  }))
+  default = [
+    { cidr = "10.0.1.0/24", availability_zone = "us-east-1a" },
+    { cidr = "10.0.2.0/24", availability_zone = "us-east-1b" },
+  ]
 }
 
-variable "private_subnet_cidrs" {
-  description = "List of CIDR blocks for private subnets"
-  type        = list(string)
-  default     = ["10.0.101.0/24", "10.0.102.0/24"]
-}
-
-variable "availability_zones" {
-  description = "List of availability zones to deploy subnets into"
-  type        = list(string)
+variable "private_subnets" {
+  description = "List of private subnet configurations, each with a cidr and availability_zone"
+  type = list(object({
+    cidr              = string
+    availability_zone = string
+  }))
+  default = [
+    { cidr = "10.0.101.0/24", availability_zone = "us-east-1a" },
+    { cidr = "10.0.102.0/24", availability_zone = "us-east-1b" },
+  ]
 }
 
 variable "tags" {
