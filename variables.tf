d8cf234eac1cab760f83b12fd383c266b220b98a
Lee Hull <ubuntu@openclaw.tail179985.ts.net>
2026-05-09 22:28:20 +0000

    feat: add basic VPC network with public and private subnets

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "name" {
+  description = "Name prefix for all resources"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones to deploy subnets into"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Additional tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
