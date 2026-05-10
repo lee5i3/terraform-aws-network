e6c6742d7a9822f93b24fdbe15e62b2c9b44cbb0
Lee Hull <ubuntu@openclaw.tail179985.ts.net>
2026-05-10 00:38:49 +0000

    feat: add Tier tag to each subnet type for cost allocation and discovery

--- subnets.tf
+++ subnets.tf
@@ -6,17 +6,10 @@ resource "aws_subnet" "public" {
   availability_zone       = var.public_subnets[count.index].availability_zone
   map_public_ip_on_launch = true
 
-  tags = merge(local.common_tags, { Name = "${var.name}-public-${count.index + 1}" })
-}
-
-resource "aws_subnet" "transit" {
-  count = length(var.transit_subnets)
-
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = var.transit_subnets[count.index].cidr
-  availability_zone = var.transit_subnets[count.index].availability_zone
-
-  tags = merge(local.common_tags, { Name = "${var.name}-transit-${count.index + 1}" })
+  tags = merge(local.common_tags, {
+    Name = "${var.name}-public-${count.index + 1}"
+    Tier = "public"
+  })
 }
 
 resource "aws_subnet" "private" {
@@ -26,5 +19,21 @@ resource "aws_subnet" "private" {
   cidr_block        = var.private_subnets[count.index].cidr
   availability_zone = var.private_subnets[count.index].availability_zone
 
-  tags = merge(local.common_tags, { Name = "${var.name}-private-${count.index + 1}" })
+  tags = merge(local.common_tags, {
+    Name = "${var.name}-private-${count.index + 1}"
+    Tier = "private"
+  })
+}
+
+resource "aws_subnet" "transit" {
+  count = length(var.transit_subnets)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.transit_subnets[count.index].cidr
+  availability_zone = var.transit_subnets[count.index].availability_zone
+
+  tags = merge(local.common_tags, {
+    Name = "${var.name}-transit-${count.index + 1}"
+    Tier = "transit"
+  })
 }
